79b81809d4a3793692171c2d417a1e0133f02115
Sanjay Soundarajan <sanjay.16@live.com>
2023-08-05 20:36:37 -0700

    ✨ feat: added a drawer for metadata viewing

--- web/src/components/cytoscape/GraphContainer.vue
+++ web/src/components/cytoscape/GraphContainer.vue
@@ -1,7 +1,80 @@
+<script setup lang="ts">
+const neuronModules = ref<string[]>([])
+const showVisualization = ref(false)
+const loading = ref(false)
+
+const { info, error } = useMessage()
+
+const submitModels = async () => {
+  loading.value = true
+  showVisualization.value = false
+
+  const data = []
+
+  for (const neuron of neuronModules.value) {
+    data.push({
+      data: {
+        id: neuron,
+        label: neuron
+      }
+    })
+  }
+
+  if (data.length === 0) {
+    error('Please add at least one neuron module')
+
+    // loading.value = false
+
+    // return
+  }
+
+  const length = neuronModules.value.length
+
+  info(`Generating visualization for ${length} neuron${length > 1 ? 's' : ''}`)
+
+  setTimeout(() => {
+    /**
+     * * Simulate the data being fetched from the server
+     */
+    loading.value = false
+    showVisualization.value = true
+  }, 10)
+}
+</script>
+
 <template>
   <div class="graph-container h-full w-full">
-    <h2 class="pb-5">Graph Container</h2>
+    <div class="flex items-center space-x-4">
+      <h2>Which neuron modules would you like to visualize?</h2>
 
-    <GraphElement />
+      <div>
+        <n-dynamic-tags v-model:value="neuronModules" type="info">
+          <template #trigger="{ activate }">
+            <n-button size="small" type="primary" dashed @click="activate()">
+              <template #icon>
+                <f-icon icon="carbon:add-filled" />
+              </template>
+
+              Add Neuron
+            </n-button>
+          </template>
+        </n-dynamic-tags>
+      </div>
+
+      <n-divider vertical />
+
+      <n-button type="info" strong size="small" :loading="loading" @click="submitModels">
+        <template #icon>
+          <f-icon icon="pixelarticons:play" />
+        </template>
+        Visualize
+      </n-button>
+    </div>
+
+    <n-collapse-transition :show="showVisualization">
+      <n-divider />
+
+      <GraphElement />
+    </n-collapse-transition>
   </div>
 </template>
--- web/src/components/cytoscape/GraphElement.vue
+++ web/src/components/cytoscape/GraphElement.vue
@@ -2,8 +2,15 @@
 import cytoscape from 'cytoscape'
 import { onMounted } from 'vue'
 
+const DrawerActive = ref(false)
+
+const DrawerData = ref({
+  title: '',
+  content: ''
+})
+
 const init = () => {
-  cytoscape({
+  const cy = cytoscape({
     container: document.getElementById('cy'),
     elements: {
       nodes: [
@@ -69,6 +76,24 @@ const init = () => {
       }
     ]
   })
+
+  cy.on('tap', 'node', (evt) => {
+    var node = evt.target
+    console.log('tapped ' + node.id())
+
+    DrawerData.value.title = `Data for node ${node.id()}`
+
+    DrawerActive.value = true
+  })
+
+  cy.on('tap', 'edge', (evt) => {
+    var edge = evt.target
+    console.log('tapped ' + edge.id())
+
+    DrawerData.value.title = `Data for edge ${edge.id()}`
+
+    DrawerActive.value = true
+  })
 }
 
 onMounted(() => {
@@ -77,5 +102,19 @@ onMounted(() => {
 </script>
 
 <template>
-  <div id="cy" class="border min-w-full h-[calc(100vh-200px)]"></div>
+  <div>
+    <div id="cy" class="border min-w-full h-[calc(100vh-200px)]"></div>
+
+    <n-drawer
+      v-model:show="DrawerActive"
+      default-width="30%"
+      placement="right"
+      :resizable="true"
+      :auto-focus="true"
+    >
+      <n-drawer-content :title="DrawerData.title" closable>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Quisquam, quos.
+      </n-drawer-content>
+    </n-drawer>
+  </div>
 </template>
